95f930404ec1c396b7dca92909df7eafd2a98fc3
kshiitz1111 <kshitizghimire8@gmail.com>
2023-07-27 23:58:34 +0800

    getURLsFromHTML and test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,30 @@
+const {JSDOM} = require('jsdom')
+
+function getUrlsFromHTML(htmlBody, baseUrl){
+   const urls = []
+   const dom = new JSDOM(htmlBody)
+   const linkElements = dom.window.document.querySelectorAll('a')
+   for(const linkElement of linkElements){
+      if(linkElement.href.slice(0, 1) === '/'){
+         try {
+            const urlObj = new URL(`${baseUrl}${linkElement.href}`)
+            urls.push(urlObj.href)
+         } catch (error) {
+            console.log(`error with relative url: ${error.message}`)
+         }
+      }else{
+         try {
+            const urlObj = new URL(linkElement.href)
+            urls.push(urlObj.href)
+         } catch (error) {
+            console.log(`error with absolute url: ${error.message}`)
+         }
+      }
+   }
+
+   return urls;
+}
+
 function normalizeURL(urlString){
    const url = new URL(urlString);
    const hostPath = `${url.hostname}${url.pathname}`;
@@ -8,4 +35,4 @@ function normalizeURL(urlString){
    return hostPath
 }
 
-module.exports = { normalizeURL }
+module.exports = { normalizeURL, getUrlsFromHTML }
