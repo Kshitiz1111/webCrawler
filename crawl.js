884c3e6c23bffc43e0a38e740cc68138d4194f37
kshiitz1111 <kshitizghimire8@gmail.com>
2023-07-30 00:10:15 +0800

    crawled all links with having same hostname

--- crawl.js
+++ crawl.js
@@ -1,5 +1,51 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(baseURL, currentURL, pagesObj){
+   
+   const baseURLObj = new URL(baseURL);
+   const currentURLObj = new URL(currentURL);
+   if(baseURLObj.hostname !== currentURLObj.hostname){
+      return pagesObj
+   }
+
+   const normalizeCurrentURL = normalizeURL(currentURL);
+   if(pagesObj[normalizeCurrentURL] > 0){
+      pagesObj[normalizeCurrentURL]++
+      return pagesObj;
+   }
+   pagesObj[normalizeCurrentURL] = 1
+   console.log(`actively crawling: ${currentURL}`)
+
+
+   try {
+      const resp = await fetch(currentURL);
+
+      if(resp.status > 399){
+         console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+         return pagesObj
+      }
+
+      const contentType = resp.headers.get("content-type");
+
+      if(!contentType.includes("text/html")){
+         console.log(`non html response, content type: ${contentType}, on page ${currentURL}`)
+         return pagesObj
+      }
+
+      const htmlBody = await resp.text()
+
+      const nextURLs = getUrlsFromHTML(htmlBody, baseURL)
+
+      for(const nextURL of nextURLs){
+         pagesObj = await crawlPage(baseURL, nextURL, pagesObj)
+      }
+   } catch (error) {
+      console.log(`error unvalid URL: ${error.message}, on page: ${currentURL}`)
+   }
+
+   return pagesObj
+}
+
 function getUrlsFromHTML(htmlBody, baseUrl){
    const urls = []
    const dom = new JSDOM(htmlBody)
@@ -35,4 +81,4 @@ function normalizeURL(urlString){
    return hostPath
 }
 
-module.exports = { normalizeURL, getUrlsFromHTML }
+module.exports = { normalizeURL, getUrlsFromHTML, crawlPage }
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl");
+
+async function main(){
+   //why length 3?
+   //because argv[0] is program interpetor or compiler in our case it is node
+   //argv[1] is a path to the file that we are running
+   //argv[2] is the argument provided
+   if(process.argv.length < 3){
+      console.log("no website provide");
+      process.exit(1)
+   }
+   //we cannot pass more that one command line argument 
+   if(process.argv.length > 3){
+      console.log("too many command line args");
+      process.exit(1)
+   }
+
+   const baseURL = process.argv[2];
+   console.log(`starting carwl of: ${baseURL}`)
+
+   const pages = await crawlPage(baseURL, baseURL, {})
+   /**
+    * The Object.entries() static method returns an
+    * array of a given object's own enumerable string-keyed
+    * property key-value pairs.
+    */
+   for(const page of Object.entries(pages)){
+      console.log(page)
+   }
+}
+
+main();
